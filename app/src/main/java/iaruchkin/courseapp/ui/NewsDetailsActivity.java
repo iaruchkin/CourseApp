302e4b3841e139ef7b9d90d22386f06deb8e496f
iaruchkin <iaruchkin@gmail.com>
2019-01-17 04:11:45 +0300

    задание 6 вариант без webview

--- app/src/main/java/iaruchkin/courseapp/ui/NewsDetailsActivity.java
+++ app/src/main/java/iaruchkin/courseapp/ui/NewsDetailsActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.util.Preconditions;
 
 import iaruchkin.courseapp.R;
@@ -20,22 +23,35 @@ public class NewsDetailsActivity extends AppCompatActivity {
     static final String EXTRA_NEWS_ID = "extra:newsID";
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
-    WebView mWebView;
+//    WebView mWebView;
+    ImageView mImgFull;
+    TextView mTitle;
+    TextView mDate;
+    TextView mFullText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_news_details_webview);
         setContentView(R.layout.activity_news_details);
 
         String newsId = getIntent().getStringExtra(EXTRA_NEWS_ID);
-        mWebView = findViewById(R.id.web_view);
-
+//        mWebView = findViewById(R.id.web_view);
+        mImgFull = findViewById(R.id.img_full);
+        mTitle = findViewById(R.id.item_title);
+        mDate = findViewById(R.id.item_date);
+        mFullText = findViewById(R.id.item_text);
         final ActionBar ab = getSupportActionBar();
         final ActionBar actionBar = Preconditions.checkNotNull(ab);
 
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         loadById(newsId);
+    }
+
+    protected void onStop() {
+        super.onStop();
+        compositeDisposable.clear();
     }
 
     @Override
@@ -56,7 +72,13 @@ public class NewsDetailsActivity extends AppCompatActivity {
 
         setTitle(newsItem.getCategory());
 
-        mWebView.loadUrl(newsItem.getUrl());
+        Glide.with(this).load(newsItem.getImageUrl()).into(mImgFull);
+        mTitle.setText(newsItem.getTitle());
+        mDate.setText(newsItem.getPublishDate());
+        mFullText.setText(newsItem.getPreviewText());
+//        mFullText.setText(newsItem.getFullText());
+
+//        mWebView.loadUrl(newsItem.getUrl());
     }
 
 }
