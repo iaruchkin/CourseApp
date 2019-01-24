06fa40e32746b5f99c45bc68aaf71b13c8fbcc92
iaruchkin <iaruchkin@gmail.com>
2019-01-24 04:23:41 +0300

    задание 7 сделал интро получше

--- app/src/main/java/iaruchkin/courseapp/ui/IntroActivity.java
+++ app/src/main/java/iaruchkin/courseapp/ui/IntroActivity.java
@@ -7,29 +7,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.concurrent.TimeUnit;
-
 import iaruchkin.courseapp.R;
 import iaruchkin.courseapp.ui.intro.IntroFragment;
 import iaruchkin.courseapp.ui.intro.IntroFragment2;
-import io.reactivex.Completable;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
+import me.relex.circleindicator.CircleIndicator;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class IntroActivity extends FragmentActivity {
 
-    private static final int NUM_PAGES = 2;
+    private static final int NUM_PAGES = 3;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
     private Button btnSkip, btnNext;
@@ -37,7 +31,7 @@ public class IntroActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.intro_layout);
 
         if (!Storage.needToShowIntro(this.getApplicationContext())) {
             startSecondActivity();
@@ -47,7 +41,6 @@ public class IntroActivity extends FragmentActivity {
         mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
         mPager.addOnPageChangeListener(viewPagerPageChangeListener);
-
 
         btnSkip = findViewById(R.id.btn_skip);
         btnNext = findViewById(R.id.btn_next);
@@ -63,13 +56,15 @@ public class IntroActivity extends FragmentActivity {
                 startSecondActivity();
             }
         });
+
+        CircleIndicator indicator = findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
     }
 
     ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
 
         @Override
         public void onPageSelected(int position) {
-//                addBottomDots(position);
 
             if (position == NUM_PAGES - 1) {
                 btnNext.setText(getString(R.string.start));
@@ -102,26 +97,29 @@ public class IntroActivity extends FragmentActivity {
         Log.e("intro", "onStop");
     }
 
-    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public static class ViewPagerAdapter extends FragmentStatePagerAdapter {
         public ViewPagerAdapter(FragmentManager fm) {
             super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    return new IntroFragment();
-                case 1:
-                    return new IntroFragment2();
-            }
-            return null;
         }
 
         @Override
         public int getCount() {
             return NUM_PAGES;
         }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return IntroFragment.newInstance(R.drawable.intro_screen_01, R.string.intro_page1_title, R.color.bg_screen1);
+                case 1:
+                    return IntroFragment.newInstance(R.drawable.intro_screen_02, R.string.intro_page2_title, R.color.bg_screen2);
+                case 2:
+                    return IntroFragment.newInstance(R.drawable.intro_screen_03, R.string.intro_page3_title, R.color.bg_screen3);
+            }
+            return null;
+        }
+
     }
 
 }
--- app/src/main/java/iaruchkin/courseapp/ui/intro/IntroFragment.java
+++ app/src/main/java/iaruchkin/courseapp/ui/intro/IntroFragment.java
@@ -5,17 +5,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import iaruchkin.courseapp.R;
 
 public class IntroFragment extends Fragment {
 
+    private int title, color, image;
+
+    // newInstance constructor for creating fragment with arguments
+    public static IntroFragment newInstance(int image, int title, int color) {
+        IntroFragment fragmentFirst = new IntroFragment();
+        Bundle args = new Bundle();
+        args.putInt("image", image);
+        args.putInt("title", title);
+        args.putInt("pageColor", color);
+        fragmentFirst.setArguments(args);
+        return fragmentFirst;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        image = getArguments().getInt("image", 0);
+        title = getArguments().getInt("title",0);
+        color = getArguments().getInt("pageColor", R.color.bg_screen1);
+    }
+
         @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.intro_fragment, container, false);
 
-        return rootView;
+            View view = inflater.inflate(R.layout.intro_fragment, container, false);
+            TextView tvLabel = view.findViewById(R.id.page_title);
+            ImageView imageView = view.findViewById(R.id.screenshot_img);
+
+            view.setBackgroundColor(getResources().getColor(color));
+            imageView.setImageResource(image);
+            tvLabel.setText(getResources().getText(title));
+
+        return view;
     }
 }
